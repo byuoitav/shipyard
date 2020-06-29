a1f7b0ba5cb4140b56ec205aa8786d1299c4f229
Brayden Winterton <bwinterton@gmail.com>
2020-06-29 15:44:17 -0600

    Update Device objects to match new structures

--- device.go
+++ device.go
@@ -2,19 +2,23 @@ package shipyard
 
 // Room represents a physical room containing AV Equipment
 type Room struct {
-	ID          string `json:"id"`
-	Designation string `json:"designation"`
-	Tags        Tags   `json:"tags"`
+	ID           string `json:"id"`
+	Designation  string `json:"designation"`
+	Description  string `json:"description"`
+	Notes        string `json:"notes"`
+	ProxyBaseURL string `json:"proxyBaseURL"`
+	Tags         Tags   `json:"tags"`
 }
 
 // Device represents a physical AV device and its configuration
 type Device struct {
-	ID      string       `json:"id"`
-	Address string       `json:"address"`
-	TypeID  string       `json:"typeID"`
-	Proxy   ProxyConfig  `json:"proxy"`
-	Ports   []DevicePort `json:"ports"`
-	Tags    Tags         `json:"tags"`
+	ID           string       `json:"id"`
+	Description  string       `json:"description"`
+	Address      string       `json:"address"`
+	Driver       string       `json:"driver"`
+	DyanmicPorts bool         `json:"dynamicPorts"`
+	Ports        []DevicePort `json:"ports"`
+	Tags         Tags         `json:"tags"`
 }
 
 // DeviceTemplate represents a template for a device
@@ -25,14 +29,12 @@ type DeviceTemplate struct {
 
 // DevicePort represents a port on a physical device
 type DevicePort struct {
+	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Endpoint string `json:"endpoint"`
 	Incoming bool   `json:"incoming"`
 	Type     string `json:"type"`
 }
-
-// ProxyConfig is a mapping of host patterns to proxy hosts for proxying calls
-type ProxyConfig map[string]string
 
 // Tags are a set of arbitrary key value pairs used for storing extra information
 type Tags map[string]string
