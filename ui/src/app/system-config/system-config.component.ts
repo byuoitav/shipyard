68a7ea7b7cb882f4952281ac817477b5e192a890
Joe <joseph.as.peterson@gmail.com>
2021-06-24 15:44:00 -0600

    change system config pages to grid layout and add header fixes #32

--- ui/src/app/system-config/system-config.component.ts
+++ ui/src/app/system-config/system-config.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { System } from '../services/system';
 
 @Component({
   selector: 'app-system-config',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./system-config.component.scss']
 })
 export class SystemConfigComponent implements OnInit {
+  system: System = new System();
 
-  constructor() {}
+
+  constructor(private route: ActivatedRoute,
+    private api: ApiService) {
+    var systemID = Number(this.route.snapshot.paramMap.get('systemID'));
+    this.system = this.api.getSystemByID(systemID);
+  }
 
   ngOnInit(): void {}
 
